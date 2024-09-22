d19a83809dd4278756a798e76178befdfe97b996
Your Name <k.bristi057@gmail.com>
2024-09-22 10:36:04 +0600

    added  some part of js

--- js/home.js
+++ js/home.js
@@ -1,3 +1,18 @@
 document.getElementById('menu-btn').addEventListener('click', function(){
     document.getElementById('menu-option').classList.remove('hidden');
 })
+
+const seatSelected=document.getElementById('seat-selected');
+
+function handleSelectionSeat(event){
+    event.classList.add('bg-bg-primary');
+    event.classList.add('text-white');
+
+    // console.log(event.innerText);
+    // seatSelected.innerHTML = `<li class="text-base font-normal flex justify-between">
+    // <span>${ event.innerText }</span>
+    //     <span>Economy</span>
+    //     <span>550</span>
+
+    // </li> `;
+}
